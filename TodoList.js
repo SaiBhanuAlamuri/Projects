c152684551cf0930916ff9213a9b5ea87d9fc94f
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-11-05 16:00:15 +0530

    Create TodoList.js

---
+++ TodoList.js
@@ -0,0 +1,49 @@
+const inputBox=document.getElementById("input-box");
+const listcontainer=document.getElementById("list-conatiner");
+function addtask()
+{
+    if(inputBox.value == '')
+    {
+        alert("You must write something!");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listcontainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span)
+
+    }
+    inputBox.value= "";
+    save();
+
+
+}
+listcontainer.addEventListener("click",function(e)
+{
+    if(e.target.tagName === "LI")
+    {
+        e.target.classList.toggle("checked");
+        save();
+
+    }
+    else if(e.target.tagName === "SPAN")
+    {
+        e.target.parentElement.remove();
+        save();
+
+        
+    }
+
+},false);
+function save()
+{
+    localStorage.setItem("data",listContainer.innerHTML);
+
+}
+function show()
+{
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+show();
